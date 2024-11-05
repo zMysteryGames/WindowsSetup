bd976f7ffd84e24016895066e1d44c0ff12faafb
Jan Osing <91972833+JanUwU42@users.noreply.github.com>
2024-11-05 17:36:40 +0100

    Added the install script

---
+++ InstallChocoAndApps.ps1
@@ -0,0 +1,13 @@
+# Chocolatey & Apps installation script
+
+# Install Chocolatey
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install Apps
+    # List of applications to install
+    $apps = @("firefox", "discord", "spotify", "spicetify-cli", "wezterm", "winfetch", "vscodium", "vlc", "libreoffice-fresh", "brave", "translucenttb", "powertoys")
+
+    # Install each app
+    foreach ($app in $apps) {
+        choco install $app -y
+    }
